442018c7705cf318265ac494a269623022197bdf
Gwen Smuda <gwen.smuda@gmail.com>
2020-02-19 15:56:53 -0600

    need to mock request to IAM

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,14 @@ require_relative 'support/mocklist'
 RSpec.configure do |config|
   config.before(:each) do
     stub_request(:any, /milwaukee.craigslist.org/).to_rack(MockList)
+
+    stub_request(:get, "http://169.254.169.254/latest/meta-data/iam/security-credentials/").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Ruby'
+      }).
+    to_return(status: 200, body: "", headers: {})
   end
 end
